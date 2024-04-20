d11d7ae9d47f507491bd5be256423c6d14aa374e
eshaan_mahimkar <eshaan.mahimkar@student.uts.edu.au>
2024-04-20 15:44:41 +1000

    Versioning untracked files

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import NavLinks from './NavLinks';
+import NavLinks from "./NavLinks";
 
 
 function Menu(){
     return(
-        <div className="nav-links" >
+        <div className="nav-links">
           {NavLinks.map(link => (
             <a style={{ textDecoration: 'none' }} key={link.id} href={link.href}>{link.text}</a>
           ))}
